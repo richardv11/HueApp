e8d88c7dca4c6bba125133efaaa543c052ce8d42
Matthew Rodgers <ascended27@gmail.com>
2017-12-03 19:12:14 -0500

    Merged Hue stuff

--- app/src/main/java/a477/hueapp/HueHelper.java
+++ app/src/main/java/a477/hueapp/HueHelper.java
@@ -1,8 +1,16 @@
 package a477.hueapp;
 
-/**
- * Created by mrodger4 on 11/20/17.
- */
+import com.philips.lighting.hue.sdk.PHHueSDK;
 
 public class HueHelper {
+
+    private final PHHueSDK phHueSDK;
+        private static final String appName = "";
+
+    public HueHelper(){
+        phHueSDK = PHHueSDK.create();
+        phHueSDK.setAppName("");     // e.g. phHueSDK.setAppName("QuickStartApp");
+        phHueSDK.setDeviceName("");  // e.g. If you are programming for Android: phHueSDK.setDeviceName(android.os.Build.MODEL);
+    }
+
 }
