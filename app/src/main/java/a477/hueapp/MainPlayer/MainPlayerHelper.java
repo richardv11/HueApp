d1fad29153fbb24e8eec7198c561842369aa41b6
Matthew Rodgers <ascended27@gmail.com>
2017-12-16 20:43:36 -0500

    Removed un-needed main player wrapper thread

--- app/src/main/java/a477/hueapp/MainPlayer/MainPlayerHelper.java
+++ app/src/main/java/a477/hueapp/MainPlayer/MainPlayerHelper.java
@@ -3,7 +3,6 @@ package a477.hueapp.MainPlayer;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.philips.lighting.model.PHLight;
 
@@ -14,6 +13,12 @@ import a477.hueapp.hue.HueHelperException;
 import a477.hueapp.savedRuns.SavedRunStateManager;
 import a477.hueapp.savedRuns.SavedRunStates;
 import a477.hueapp.savedRuns.SavedRunsHelper;
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class MainPlayerHelper {
 
@@ -23,7 +28,8 @@ public class MainPlayerHelper {
     private HueHelper hueHelper;
     private SavedRunsHelper srHelper;
     private SQLiteDatabase db;
-    private Context context;
+    private AudioDispatcher dispatcher;
+    private double pitchInHz;
 
     private MainPlayerHelper(Context context) {
         hueHelper = HueHelper.getInstance();
@@ -31,7 +37,6 @@ public class MainPlayerHelper {
         db = srHelper.getWritableDatabase();
         playerStateManager = PlayerStateManager.getInstance();
         srStateManager = SavedRunStateManager.getInstance();
-        this.context = context;
     }
 
     public static MainPlayerHelper getInstance(Context context) {
@@ -59,19 +64,16 @@ public class MainPlayerHelper {
                     }
                     // Change state to playing
                     playerStateManager.playerStarted();
-
-                    MainPlayerRunner runner = new MainPlayerRunner(context);
-                    Thread mainPlayerThread = new Thread(runner);
-                    playerStateManager.setMainPlayerThread(mainPlayerThread);
-                    mainPlayerThread.start();
+                    createThread();
 
                 } else {
-                    // TODO: Warn user that the saved run player must be stopped before starting a saved run.
+                    // TODO: Warn user that the saved run player must be stopped before starting a saved run?
 
                 }
             }
         } else {
-            Toast.makeText(context, "Please enable a light first", Toast.LENGTH_SHORT).show();
+            // TODO: Notify the user that a light is needed?
+            // Toast.makeText(this, "Please enable a light first", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -103,5 +105,60 @@ public class MainPlayerHelper {
         }
     }
 
+    private void createThread() {
+        // 22050, 1024, 0
+        dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(48000, 8000, 0);
+        PitchDetectionHandler handler = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult result, AudioEvent e) {
+                // Only do stuff if we are playing
+                if (playerStateManager.getState().equals(PlayerState.PLAYING)) {
+                    // Get the pitch
+                    pitchInHz = result.getPitch();
+                    if (pitchInHz != -1) {
+                        try {
+                            // If we change our state before we start processing then interrupt ourself
+                            if (!playerStateManager.getState().equals(PlayerState.PLAYING)) {
+                                throw new InterruptedException("Thread is stopped/paused");
+                            }
+                            Log.i("TARSOS_PITCH", String.valueOf(pitchInHz));
 
+                            // Try and change the light
+                            try {
+                                // Add a new note to the current run
+                                srHelper.addNote("" + pitchInHz + " " + System.currentTimeMillis());
+
+                                // Change the light
+                                PHLight light = hueHelper.getNextLight();
+                                if (light != null) {
+                                    hueHelper.setSaturation(light, 150);
+                                    hueHelper.setHue(light, (int) (pitchInHz * 1000) % 65535);
+                                }
+                            } catch (HueHelperException e2) {
+                                // Something went wrong with the note change
+                                Log.e("HUE APP", "handlePitch: ", e2);
+                            }
+                        } catch (InterruptedException e3) {
+                            // Our state changed
+                            if (playerStateManager.getState().equals(PlayerState.STOPPED)) {
+                                // We got stopped so we need to stop listening.
+                                if (dispatcher != null) {
+                                    dispatcher.stop();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        };
+
+        // Add the chosen audio processor to the dispatcher
+        PitchProcessor processor = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 48000, 8000, handler);
+        dispatcher.addAudioProcessor(processor);
+
+        // Start the thread
+        Thread mainPlayerThread = new Thread(dispatcher, "Audio Dispatcher");
+        playerStateManager.setMainPlayerThread(mainPlayerThread);
+        mainPlayerThread.start();
+    }
 }
